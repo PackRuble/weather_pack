c8a4bea1ae5603288f9e3417309221cb68ae20be
Ruble <pack.ruble@gmail.com>
2023-07-28 11:59:37 +0300

    add: constructor `OWMApiTest` now have param `client`

--- lib/src/api/api_testing.dart
+++ lib/src/api/api_testing.dart
@@ -5,9 +5,9 @@ import 'api.dart';
 /// A class to help check your APIkey.
 class OWMApiTest {
   // ignore: public_member_api_docs
-  OWMApiTest();
+  OWMApiTest([http.Client? client]) : _client = client ?? http.Client();
 
-  final http.Client _httpClient = http.Client();
+  final http.Client _client;
 
   static const int _statusOkTestApi = 400;
 
@@ -15,7 +15,7 @@ class OWMApiTest {
   Future<bool> isCorrectApiKey(String yourApi) async {
     final Uri uri = OWMApi.uriTestApikey(yourApi);
 
-    final http.Response response = await _httpClient.get(uri);
+    final http.Response response = await _client.get(uri);
 
     if (response.statusCode == _statusOkTestApi) {
       return true;
