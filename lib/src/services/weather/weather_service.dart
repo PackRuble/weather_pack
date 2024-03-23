ee6ee32f1c73609399b4dd2eec148d8f63d7575f
Ruble <pack.ruble@gmail.com>
2024-03-23 15:56:46 +0300

    add: `WeatherService` now support new param - `oneCallApi`. Also replace `OWMApi` on `CurrentWeatherApi` and `OneCallWeatherApi`

--- lib/src/services/weather/weather_service.dart
+++ lib/src/services/weather/weather_service.dart
@@ -1,26 +1,42 @@
-import 'package:meta/meta.dart';
+import 'package:weather_pack/src/api/current_weather_api.dart';
 
-import '../../api/api.dart';
+import '../../api/onecall_api.dart';
 import '../../utils/languages.dart';
 import '../checks.dart';
 import '../ovm_builder.dart';
 import 'all_weather_model.dart';
 
-/// Service for fetching weather data in JSON.
+/// Service for fetching weather data:
+/// - [WeatherOneCall]
+/// - [WeatherCurrent]
 class WeatherService {
   WeatherService(
-    String api, {
-    WeatherLanguage? language,
-    @visibleForTesting OWMBuilder? owmBuilder,
-  })  : _owmApi = OWMApi(api, language: language ?? WeatherLanguage.english),
+    String apikey, {
+    WeatherLanguage language = WeatherLanguage.english,
+
+    /// The new api key may not fit One Call Api 2.5 and OVER.
+    ///
+    /// If you have previously used the One Call Api 2.5 key and service, use [OneCallApi.api_2_5]
+    /// Link: https://openweathermap.org/api/one-call-api
+    ///
+    /// If you use the "One Call by Call" subscription and One Call API 3.0, then use [OneCallApi.api_3_0]
+    /// Link: https://openweathermap.org/api/one-call-3
+    ///
+    /// This does not in any way affect the use of the "Current weather data" API.
+    /// Link: https://openweathermap.org/current
+    OneCallApi oneCallApi = OneCallApi.api_2_5,
+    OWMBuilder? owmBuilder,
+  })  : _currentWeatherApi = CurrentWeatherApi(apikey, language: language),
+        _oneCallWeatherApi = OneCallWeatherApi(apikey,
+            language: language, oneCallApi: oneCallApi),
         _owmBuilder = owmBuilder ?? OWMBuilder();
 
-  final OWMApi _owmApi;
+  final CurrentWeatherApi _currentWeatherApi;
+  final OneCallWeatherApi _oneCallWeatherApi;
   final OWMBuilder _owmBuilder;
 
   /// Fetch [WeatherOneCall] based on geographical coordinates.
   ///
-  /// Result is JSON.
   /// For API documentation, see: [WeatherOnecall](https://openweathermap.org/api/one-call-api)
   Future<WeatherOneCall> oneCallWeatherByLocation({
     required double latitude,
@@ -28,16 +44,14 @@ class WeatherService {
   }) async {
     checkCoordinates(latitude, longitude);
     return _owmBuilder.getData(
-      uri: _owmApi.uriOnecallWeather(latitude, longitude),
-      builder: (dynamic data) {
-        return WeatherOneCall.fromJson(data as Map<String, dynamic>);
-      },
+      uri: _oneCallWeatherApi.uriOneCallWeather(latitude, longitude),
+      builder: (dynamic data) =>
+          WeatherOneCall.fromJson(data as Map<String, dynamic>),
     );
   }
 
   /// Fetch [WeatherCurrent] based on geographical coordinates.
   ///
-  /// Result is JSON.
   /// For API documentation, see: [WeatherCurrent](https://openweathermap.org/current)
   Future<WeatherCurrent> currentWeatherByLocation({
     required double latitude,
@@ -45,15 +59,10 @@ class WeatherService {
   }) async {
     checkCoordinates(latitude, longitude);
     return _owmBuilder.getData(
-      uri: _owmApi.uriCurrentWeather(latitude, longitude),
-      builder: (dynamic data) {
-        return WeatherCurrent.fromJson(
-          parseCurrent(data as Map<String, dynamic>),
-        );
-      },
+      uri: _currentWeatherApi.uriCurrentWeather(latitude, longitude),
+      builder: (dynamic data) => WeatherCurrent.fromJson(
+        parseCurrent(data as Map<String, dynamic>),
+      ),
     );
   }
-
-  // todo: the ability to get the weather by the name of the place - built-in geocoding
-  //  we take the first location - it is usually the most accurate
 }
