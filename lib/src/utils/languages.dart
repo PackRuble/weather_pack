434f648e0d45ec124a108cccbeaad21b5852ba10
Ruble <pack.ruble@gmail.com>
2024-03-24 14:59:51 +0300

    upd: `codeAndLangMatching` now getter

--- lib/src/utils/languages.dart
+++ lib/src/utils/languages.dart
@@ -66,68 +66,70 @@ enum WeatherLanguage {
 }
 
 /// Map of matching [WeatherLanguage.code] and [WeatherLanguage].
-final Map<String, WeatherLanguage> codeAndLangMatching =
+Map<String, WeatherLanguage> get codeAndLangMatching =>
     <String, WeatherLanguage>{
-  WeatherLanguage.afrikaans.code: WeatherLanguage.afrikaans,
-  WeatherLanguage.albanian.code: WeatherLanguage.albanian,
-  WeatherLanguage.arabic.code: WeatherLanguage.arabic,
-  WeatherLanguage.azerbaijani.code: WeatherLanguage.azerbaijani,
-  WeatherLanguage.bulgarian.code: WeatherLanguage.bulgarian,
-  WeatherLanguage.catalan.code: WeatherLanguage.catalan,
-  WeatherLanguage.czech.code: WeatherLanguage.czech,
-  WeatherLanguage.danish.code: WeatherLanguage.danish,
-  WeatherLanguage.german.code: WeatherLanguage.german,
-  WeatherLanguage.greek.code: WeatherLanguage.greek,
-  WeatherLanguage.english.code: WeatherLanguage.english,
-  WeatherLanguage.basque.code: WeatherLanguage.basque,
+      WeatherLanguage.afrikaans.code: WeatherLanguage.afrikaans,
+      WeatherLanguage.albanian.code: WeatherLanguage.albanian,
+      WeatherLanguage.arabic.code: WeatherLanguage.arabic,
+      WeatherLanguage.azerbaijani.code: WeatherLanguage.azerbaijani,
+      WeatherLanguage.bulgarian.code: WeatherLanguage.bulgarian,
+      WeatherLanguage.catalan.code: WeatherLanguage.catalan,
+      WeatherLanguage.czech.code: WeatherLanguage.czech,
+      WeatherLanguage.danish.code: WeatherLanguage.danish,
+      WeatherLanguage.german.code: WeatherLanguage.german,
+      WeatherLanguage.greek.code: WeatherLanguage.greek,
+      WeatherLanguage.english.code: WeatherLanguage.english,
+      WeatherLanguage.basque.code: WeatherLanguage.basque,
 
-  // farsi.code == persian.code
-  WeatherLanguage.farsi.code: WeatherLanguage.farsi,
-  WeatherLanguage.persian.code: WeatherLanguage.persian,
+      // farsi.code == persian.code
+      WeatherLanguage.farsi.code: WeatherLanguage.farsi,
+      WeatherLanguage.persian.code: WeatherLanguage.persian,
 
-  WeatherLanguage.finnish.code: WeatherLanguage.finnish,
-  WeatherLanguage.french.code: WeatherLanguage.french,
-  WeatherLanguage.galician.code: WeatherLanguage.galician,
-  WeatherLanguage.hebrew.code: WeatherLanguage.hebrew,
-  WeatherLanguage.hindi.code: WeatherLanguage.hindi,
-  WeatherLanguage.croatian.code: WeatherLanguage.croatian,
-  WeatherLanguage.hungarian.code: WeatherLanguage.hungarian,
-  WeatherLanguage.indonesian.code: WeatherLanguage.indonesian,
-  WeatherLanguage.italian.code: WeatherLanguage.italian,
-  WeatherLanguage.japanese.code: WeatherLanguage.japanese,
-  WeatherLanguage.korean.code: WeatherLanguage.korean,
-  WeatherLanguage.latvian.code: WeatherLanguage.latvian,
-  WeatherLanguage.lithuanian.code: WeatherLanguage.lithuanian,
-  WeatherLanguage.macedonian.code: WeatherLanguage.macedonian,
-  WeatherLanguage.norwegian.code: WeatherLanguage.norwegian,
-  WeatherLanguage.dutch.code: WeatherLanguage.dutch,
-  WeatherLanguage.polish.code: WeatherLanguage.polish,
-  WeatherLanguage.portuguese.code: WeatherLanguage.portuguese,
-  WeatherLanguage.portugueseBrazilian.code: WeatherLanguage.portugueseBrazilian,
-  WeatherLanguage.romanian.code: WeatherLanguage.romanian,
-  WeatherLanguage.russian.code: WeatherLanguage.russian,
+      WeatherLanguage.finnish.code: WeatherLanguage.finnish,
+      WeatherLanguage.french.code: WeatherLanguage.french,
+      WeatherLanguage.galician.code: WeatherLanguage.galician,
+      WeatherLanguage.hebrew.code: WeatherLanguage.hebrew,
+      WeatherLanguage.hindi.code: WeatherLanguage.hindi,
+      WeatherLanguage.croatian.code: WeatherLanguage.croatian,
+      WeatherLanguage.hungarian.code: WeatherLanguage.hungarian,
+      WeatherLanguage.indonesian.code: WeatherLanguage.indonesian,
+      WeatherLanguage.italian.code: WeatherLanguage.italian,
+      WeatherLanguage.japanese.code: WeatherLanguage.japanese,
+      WeatherLanguage.korean.code: WeatherLanguage.korean,
+      WeatherLanguage.latvian.code: WeatherLanguage.latvian,
+      WeatherLanguage.lithuanian.code: WeatherLanguage.lithuanian,
+      WeatherLanguage.macedonian.code: WeatherLanguage.macedonian,
+      WeatherLanguage.norwegian.code: WeatherLanguage.norwegian,
+      WeatherLanguage.dutch.code: WeatherLanguage.dutch,
+      WeatherLanguage.polish.code: WeatherLanguage.polish,
+      WeatherLanguage.portuguese.code: WeatherLanguage.portuguese,
+      WeatherLanguage.portugueseBrazilian.code:
+          WeatherLanguage.portugueseBrazilian,
+      WeatherLanguage.romanian.code: WeatherLanguage.romanian,
+      WeatherLanguage.russian.code: WeatherLanguage.russian,
 
-  // swedish.code == 'sv'
-  WeatherLanguage.swedish.code: WeatherLanguage.swedish,
-  'se': WeatherLanguage.swedish, // too swedish
+      // swedish.code == 'sv'
+      WeatherLanguage.swedish.code: WeatherLanguage.swedish,
+      'se': WeatherLanguage.swedish, // too swedish
 
-  WeatherLanguage.slovak.code: WeatherLanguage.slovak,
-  WeatherLanguage.slovenian.code: WeatherLanguage.slovenian,
+      WeatherLanguage.slovak.code: WeatherLanguage.slovak,
+      WeatherLanguage.slovenian.code: WeatherLanguage.slovenian,
 
-  // spanish.code == 'sp'
-  WeatherLanguage.spanish.code: WeatherLanguage.spanish,
-  'es': WeatherLanguage.spanish, // too spanish
+      // spanish.code == 'sp'
+      WeatherLanguage.spanish.code: WeatherLanguage.spanish,
+      'es': WeatherLanguage.spanish, // too spanish
 
-  WeatherLanguage.serbian.code: WeatherLanguage.serbian,
-  WeatherLanguage.thai.code: WeatherLanguage.thai,
-  WeatherLanguage.turkish.code: WeatherLanguage.turkish,
+      WeatherLanguage.serbian.code: WeatherLanguage.serbian,
+      WeatherLanguage.thai.code: WeatherLanguage.thai,
+      WeatherLanguage.turkish.code: WeatherLanguage.turkish,
 
-  // ukrainian.code == 'ua'
-  WeatherLanguage.ukrainian.code: WeatherLanguage.ukrainian,
-  'uk': WeatherLanguage.ukrainian, // too ukrainian
+      // ukrainian.code == 'ua'
+      WeatherLanguage.ukrainian.code: WeatherLanguage.ukrainian,
+      'uk': WeatherLanguage.ukrainian, // too ukrainian
 
-  WeatherLanguage.vietnamese.code: WeatherLanguage.vietnamese,
-  WeatherLanguage.chineseSimplified.code: WeatherLanguage.chineseSimplified,
-  WeatherLanguage.chineseTraditional.code: WeatherLanguage.chineseTraditional,
-  WeatherLanguage.zulu.code: WeatherLanguage.zulu,
-};
+      WeatherLanguage.vietnamese.code: WeatherLanguage.vietnamese,
+      WeatherLanguage.chineseSimplified.code: WeatherLanguage.chineseSimplified,
+      WeatherLanguage.chineseTraditional.code:
+          WeatherLanguage.chineseTraditional,
+      WeatherLanguage.zulu.code: WeatherLanguage.zulu,
+    };
